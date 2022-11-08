0d29b6ee18171ba6b942e9adf0e12b1a2e191a7e
Ryan <ryan@neshacker.com>
2022-11-08 18:31:37 -0500

    Removed constant defintions to keep example focused on looping.

--- examples/05_Loops.s
+++ examples/05_Loops.s
@@ -2,33 +2,30 @@
 .segment "CODE"
 
 .proc Main
-  monster_hp_table = $0300
-  attack_damage = 50
-
   ; Initialize each monster's HP
   lda #100
   ldx #7
 initialize_hp_loop:
-  sta monster_hp_table, x
+  sta $0300, x
   dex
   bpl initialize_hp_loop
 
   ; Make a couple changes to test the logic
   lda #0
-  sta monster_hp_table + 1
+  sta $0301
   lda #15
-  sta monster_hp_table + 6
+  sta $0306
 
   ; Perform the multi-attack
   ldx #0
 multi_attack_loop:
   ; If max HP is zero, skip (monster inactive)
-  lda monster_hp_table, x
+  lda $0300, x
   beq check_condition
 
   ; Subtract the attack damage
   sec
-  sbc #attack_damage
+  sbc #50
 
   ; If the result >= 0 then write it to memory
   ; Otherwise, set it to 0
@@ -36,7 +33,7 @@ multi_attack_loop:
   lda #0
 
 store_hp:
-  sta monster_hp_table, x
+  sta $0300, x
 
 check_condition:
   inx
